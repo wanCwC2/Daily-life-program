530ae3bff20db35186ca784d3c700a45ddf1fd8b
wanCwC2 <WanPcwc0630@gmail.com>
2023-02-18 08:49:29 +0800

    Store date

--- src/tax/User.java
+++ src/tax/User.java
@@ -4,6 +4,9 @@ import java.io.*;
 public class User {
     public static void main(String[] args) throws FileNotFoundException {
 
+        String[][] data = new String[5][14];
+        int line_len = 0;
+
         String line = "";
         final String delimiter = ",";
         try
@@ -21,14 +24,23 @@ public class User {
 //                System.out.println(token[0] + " | "+ token[1]+ " | "+ token[2]+ " | "+ token[3]);
 //                System.out.println(token);
                 for (int i = 0; i < token.length; i++) {
-                    System.out.print(token[i]+" | ");
+                    System.out.print(token[i] + " | ");
+                    data[line_len][i] = token[i];
                 }
                 System.out.println("");
+                line_len += 1;
             }
         }
         catch (IOException e)
         {
             e.printStackTrace();
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                System.out.print(data[i][j] + " | ");
+            }
+            System.out.println("");
         }
 
     }
