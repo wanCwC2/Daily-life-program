926252f2770a7cefed683dfcdb9d8bbdb26783e4
wanCwC2 <WanPcwc0630@gmail.com>
2023-02-18 10:18:47 +0800

    Calculate basic life

--- src/tax/User.java
+++ src/tax/User.java
@@ -24,10 +24,10 @@ public class User {
 //                System.out.println(token[0] + " | "+ token[1]+ " | "+ token[2]+ " | "+ token[3]);
 //                System.out.println(token);
                 for (int i = 0; i < token.length; i++) {
-                    System.out.print(token[i] + " | ");
+//                    System.out.print(token[i] + " | ");
                     data[line_len][i] = token[i];
                 }
-                System.out.println("");
+//                System.out.println("");
                 line_len += 1;
             }
         }
@@ -36,12 +36,30 @@ public class User {
             e.printStackTrace();
         }
 
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data[i].length; j++) {
-                System.out.print(data[i][j] + " | ");
+//        for (int i = 0; i < data.length; i++) {
+//            for (int j = 0; j < data[i].length; j++) {
+//                System.out.print(data[i][j] + " | ");
+//            }
+//            System.out.println("");
+//        }
+
+        int basic_life = 0; //基本生活差額
+//        System.out.println(Integer.parseInt(data[1][5]) * 192000);
+
+        for (int i = 1; i < data.length; i++) {
+
+            //無資料時跳過計算
+            if (data[i][0] == null){
+                continue;
             }
-            System.out.println("");
+
+            //基本生活差額
+            basic_life = Integer.parseInt(data[i][2]) * 192000 - Integer.parseInt(data[i][5]) * 132000 - (Integer.parseInt(data[i][2]) - Integer.parseInt(data[i][5])) * 88000;
+            if (basic_life < 0){
+                basic_life = 0;
+            }
         }
+        System.out.println(basic_life);
 
     }
 }
