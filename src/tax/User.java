ac8afc227300532d110855683561456cfbaf7e4c
wanCwC2 <WanPcwc0630@gmail.com>
2023-02-19 22:21:42 +0800

    Can't figure it out basic life

--- src/tax/User.java
+++ src/tax/User.java
@@ -12,12 +12,9 @@ public class User {
         try
         {
             String filePath = "D:\\Java\\FJU_TA_1112Programming\\src\\tax\\data\\test.csv";
-//            FileReader fileReader = new FileReader(filePath);
             FileInputStream fis = new FileInputStream(filePath);
             InputStreamReader isr = new InputStreamReader(fis);
             BufferedReader br = new BufferedReader(isr);
-
-//            BufferedReader reader = new BufferedReader(fileReader);
             while ((line = br.readLine()) != null)   //loops through every line until null found
             {
                 String[] token = line.split(delimiter);    // separate every token by comma
@@ -36,30 +33,34 @@ public class User {
             e.printStackTrace();
         }
 
-//        for (int i = 0; i < data.length; i++) {
-//            for (int j = 0; j < data[i].length; j++) {
-//                System.out.print(data[i][j] + " | ");
-//            }
-//            System.out.println("");
-//        }
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                System.out.print(data[i][j] + " | ");
+            }
+            System.out.println("");
+        }
 
         int basic_life = 0; //基本生活差額
-//        System.out.println(Integer.parseInt(data[1][5]) * 192000);
 
         for (int i = 1; i < data.length; i++) {
 
             //無資料時跳過計算
-            if (data[i][0] == null){
+            if (data[i][0] == null) {
                 continue;
             }
 
             //基本生活差額
             basic_life = Integer.parseInt(data[i][2]) * 192000 - Integer.parseInt(data[i][5]) * 132000 - (Integer.parseInt(data[i][2]) - Integer.parseInt(data[i][5])) * 88000;
-            if (basic_life < 0){
+            if (data[i][1] == "單身") {
+                basic_life -= 120000;
+            } else if (data[i][1] == "已婚") {
+                basic_life -= 240000;
+            }
+            if (basic_life < 0) {
                 basic_life = 0;
             }
+            System.out.println(basic_life);
         }
-        System.out.println(basic_life);
 
     }
 }
