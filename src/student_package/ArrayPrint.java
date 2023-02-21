fc4d80482d532cd2980f5088d212fae52a5d699b
wanCwC2 <WanPcwc0630@gmail.com>
2023-02-21 17:56:06 +0800

    Print array

---
+++ src/student_package/ArrayPrint.java
@@ -0,0 +1,10 @@
+package student_package;
+
+import java.util.Arrays;
+
+public class ArrayPrint {
+    public static void main(String[] args) {
+        Integer Array[][] = {{1,2},{2,3},{3,4}};
+        System.out.println(Arrays.deepToString(Array));
+    }
+}
